caeed2bf2186b8c7f1ece41845514ec9f3dfbb8b
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-11 11:14:13 +0300

    feat: create home page grid & put 2 components in it
    Create home page grid in `src/app/page.tsx` file using a
    div and tailwind grid css and put `HomePageGreeting`
    and `DatasetsOverviewBox` components in this grid to
    be the first components in the home page of the app.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import DatasetsOverviewBox from "@/components/datasets/DatasetsOverviewBox";
+import HomePageGreeting from "@/components/HomePageGreeting";
 
 export default async function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-2">
-
-    </main>
+    <div className="grid grid-cols-3 flex-1 h-fit gap-2">
+      <div className="col-span-full md:col-span-2">
+        <HomePageGreeting />
+      </div>
+      <div className="col-span-full md:col-span-1">
+        <DatasetsOverviewBox />
+      </div>
+    </div>
   );
 }
