820267be0acdcae0ca84869d4bf1a81127376715
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-12 11:35:27 +0300

    feat: create `useRecentDatasetsActivitiesState` hook
    Create `useRecentDatasetsActivitiesState` hook to manage
    the state of the databases in the  fetched data in
    `useRecentActivities` hook, This hook provides three functions:
    
    - `addToRecentDatasetsActivities` to add new dataset to the
    state of the fetched recent activities data.
    
    - `updateRecentDatasetsActivities` to update the state of
    datasets in the state of the fetched recent activities data.
    
    - `removeRecentDatasetsActivities` to remove datasets from
    the state of the fetched recent activities data.

---
+++ src/hook/recent-activities/useRecentDatasetsActivitiesState.ts
@@ -0,0 +1,84 @@
+import { useQueryClient } from "@tanstack/react-query";
+
+export default function useRecentDatasetsActivitiesState() {
+  const QueryClient = useQueryClient();
+
+  function addToRecentDatasetsActivities(datasetActivity: DatasetActivity) {
+    QueryClient.setQueryData<Activities | undefined>(
+      ["recent-activities"],
+      (preData) => {
+        if (preData) {
+          return {
+            ...preData,
+            datasetsActivities: [
+              datasetActivity,
+              ...preData.datasetsActivities.slice(0, 4),
+            ],
+          };
+        }
+        return undefined;
+      }
+    );
+  }
+
+  function updateRecentDatasetsActivities(
+    datasetId: Dataset["_id"],
+    updateData: Dataset | ((pre?: Dataset) => Dataset)
+  ) {
+    QueryClient.setQueryData<Activities | undefined>(
+      ["recent-activities"],
+      (preData) => {
+        if (preData) {
+          return {
+            datasetsActivities: preData.datasetsActivities.map((activity) => {
+              if (activity.dataset._id === datasetId) {
+                activity.dataset =
+                  typeof updateData === "function"
+                    ? updateData(activity.dataset)
+                    : updateData;
+              }
+              return activity;
+            }),
+            instructionsActivities: preData.instructionsActivities.map(
+              (activity) => {
+                if (activity.dataset._id === datasetId) {
+                  activity.dataset =
+                    typeof updateData === "function"
+                      ? updateData(activity.dataset)
+                      : updateData;
+                }
+                return activity;
+              }
+            ),
+          };
+        }
+        return undefined;
+      }
+    );
+  }
+
+  function removeRecentDatasetsActivities(datasetId: Dataset["_id"]) {
+    QueryClient.setQueryData<Activities | undefined>(
+      ["recent-activities"],
+      (preData) => {
+        if (preData) {
+          return {
+            datasetsActivities: preData.datasetsActivities.filter(
+              (activity) => activity.dataset._id !== datasetId
+            ),
+            instructionsActivities: preData.instructionsActivities.filter(
+              (activity) => activity.dataset._id !== datasetId
+            ),
+          };
+        }
+        return undefined;
+      }
+    );
+  }
+
+  return {
+    addToRecentDatasetsActivities,
+    updateRecentDatasetsActivities,
+    removeRecentDatasetsActivities,
+  };
+}
