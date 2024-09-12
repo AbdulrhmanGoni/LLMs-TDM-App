31145d97a63b0d6b691b77dd212fea1919ac1cd6
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-12 14:06:35 +0300

    refactor: `useRecentActivities` hook
    Return the whole result of `useQuery` query hook
    directly in `useRecentActivities` hook instead of
    extracting some valuse and return them.
    
    Also set `FetchError` class as the type of the errors in
    `useQuery` query hook.

--- src/hook/recent-activities/useRecentActivities.ts
+++ src/hook/recent-activities/useRecentActivities.ts
@@ -1,8 +1,9 @@
 import fetchAPI from "@/lib/fetchAPI";
+import FetchError from "@/lib/FetchError";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useRecentActivities() {
-  return useQuery<Activities>({
+  return useQuery<Activities, FetchError>({
     queryKey: ["recent-activities"],
     async queryFn() {
       const { body } = await fetchAPI<{ data: Activities }>(
