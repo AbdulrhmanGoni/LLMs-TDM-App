a22fc8b275937a8c50ab0e91e4be4f0e36397a80
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-12 15:24:12 +0300

    feat: add recent activities lists to the home page
    Add `RecentInstructionsActivities` & `RecentDatasetsActivities`
    components to the home page in `src/app/page.tsx` file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import DatasetsOverviewBox from "@/components/datasets/DatasetsOverviewBox";
 import HomePageGreeting from "@/components/HomePageGreeting";
+import RecentDatasetsActivities from "@/components/recent-activities/RecentDatasetsActivities";
+import RecentInstructionsActivities from "@/components/recent-activities/RecentInstructionsActivities";
 
 export default async function Home() {
 
@@ -10,6 +12,12 @@ export default async function Home() {
       </div>
       <div className="col-span-full md:col-span-1">
         <DatasetsOverviewBox />
+      </div>
+      <div className="col-span-full md:col-span-1">
+        <RecentDatasetsActivities />
+      </div>
+      <div className="col-span-full md:col-span-2">
+        <RecentInstructionsActivities />
       </div>
     </div>
   );
