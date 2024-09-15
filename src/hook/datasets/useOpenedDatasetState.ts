f9a41708ef1944e28142630d584666bb79dad136
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-15 10:44:52 +0300

    feat: create `useOpenedDatasetState` hook
    Create `useOpenedDatasetState` hook to provide utils
    functions to updatig the data of the set dataset in dataset
    page context.

---
+++ src/hook/datasets/useOpenedDatasetState.ts
@@ -0,0 +1,52 @@
+import { useQueryClient } from "@tanstack/react-query";
+import useDatasetPageContext from "./useDatasetPageContext";
+
+export default function useOpenedDatasetState() {
+  const QueryClient = useQueryClient();
+
+  const { setDataset, dataset } = useDatasetPageContext();
+
+  function updateDataset(
+    datasetId: Dataset["_id"],
+    updateData: Dataset | ((pre?: Dataset | null) => Dataset) | null
+  ) {
+    QueryClient.setQueryData<Dataset | null>(
+      ["dataset", datasetId],
+      updateData
+    );
+    if (dataset?._id === datasetId) {
+      setDataset(updateData);
+    }
+  }
+
+  function updateDatasetInstructionsCount(
+    datasetId: Dataset["_id"],
+    amount: number
+  ) {
+    QueryClient.setQueryData<Dataset | undefined>(
+      ["dataset", datasetId],
+      (dataset) => {
+        return dataset && updateDatasetInstructionsCountFn(dataset, amount);
+      }
+    );
+    if (dataset?._id === datasetId) {
+      setDataset((dataset) => {
+        return dataset && updateDatasetInstructionsCountFn(dataset, amount);
+      });
+    }
+  }
+
+  return {
+    updateDataset,
+    updateDatasetInstructionsCount,
+  };
+}
+
+function updateDatasetInstructionsCountFn(dataset: Dataset, amount: number) {
+  if (dataset) {
+    return {
+      ...dataset,
+      instructionsCount: dataset.instructionsCount + amount,
+    };
+  } else return dataset;
+}
