8117937972f23728455bd5c6ff3d040df5a1fc49
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-16 21:20:52 +0300

    refacter: rename three components
    - Rename `DatasetOverviewBoxLoading` component to
    `DatasetsOverviewBoxLoading`
    
    - Rename `DatasetOverviewBoxError` component to
    `DatasetsOverviewBoxError`.
    
    - Rename `DatasetOverviewBox` component to `DatasetsOverviewBox`.
    
    Just make "dataset" word plural.

--- src/components/datasets/DatasetsOverviewBox.tsx
+++ src/components/datasets/DatasetsOverviewBox.tsx
@@ -3,17 +3,17 @@ import useDatasetsOverview from '@/hook/datasets/useDatasetsOverview';
 import { Card, CardTitle } from '../ui/card'
 import { DatabaseIcon } from 'lucide-react'
 import { useRouter } from 'next/navigation';
-import DatasetOverviewBoxLoading from './DatasetOverviewBoxLoading';
-import DatasetOverviewBoxError from './DatasetOverviewBoxError';
+import DatasetsOverviewBoxLoading from './DatasetsOverviewBoxLoading';
+import DatasetsOverviewBoxError from './DatasetsOverviewBoxError';
 
-export default function DatasetOverviewBox() {
+export default function DatasetsOverviewBox() {
 
     const { data, isFetching, error, refetch } = useDatasetsOverview()
     const { push } = useRouter()
 
     return (
-        isFetching ? <DatasetOverviewBoxLoading /> :
-            error ? <DatasetOverviewBoxError error={error} refetch={refetch} /> : (
+        isFetching ? <DatasetsOverviewBoxLoading /> :
+            error ? <DatasetsOverviewBoxError error={error} refetch={refetch} /> : (
                 <Card
                     className='flex flex-col justify-center flex-1 h-full p-6 cursor-pointer'
                     onClick={() => push("/datasets")}
