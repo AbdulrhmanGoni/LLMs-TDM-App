9f813ea4e8ec9fdc730783b5abe2d85a81854560
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-17 13:52:43 +0300

    styles: make the bg color of `Greeting` component darker

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -9,7 +9,7 @@ type GreetingProps = {
 
 export default function Greeting({ greetingMessage, description, illutrationImageSrc }: GreetingProps) {
     return (
-        <Card className='flex p-2 gap-2 items-center justify-between w-full relative bg-[#2196F3]'>
+        <Card className='flex p-2 gap-2 items-center justify-between w-full relative bg-[#006abe]'>
             <div className='p-1.5'>
                 <h1 className='text-2xl md:text-3xl mb-1'>{greetingMessage}</h1>
                 <p className='text-base md:text-lg'>{description.map((text) => <>{text}<br /></>)}</p>
