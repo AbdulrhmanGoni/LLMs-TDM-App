28353b01db5fe6163d98f1615d2fcf95e03d5841
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-20 23:41:16 +0300

    refactor: create `RecentActivitiesList` component
    Take the shared parts between `RecentDatasetsActivities`
    and `RecentInstructionsActivities` components ( actually
    most parts) to create `RecentActivitiesList` component
    which should be used to build the two components
    (`RecentDatasetsActivities` and `RecentInstructionsActivities`).

---
+++ src/components/recent-activities/RecentActivitiesList.tsx
@@ -0,0 +1,46 @@
+"use client"
+import useRecentActivities from '@/hook/recent-activities/useRecentActivities';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { DatabaseZapIcon } from 'lucide-react'
+import RecentDatasetsActivitiesError from './RecentActivitiesError';
+import { Separator } from '../ui/separator';
+import { JSX } from 'react';
+
+type RecentActivitiesListProps = {
+    resource: keyof Activities
+    LoadingListComponent: JSX.Element
+    ActivityCard: (list: DatasetActivity[] | InstructionActivity[]) => JSX.Element[]
+}
+
+export default function RecentActivitiesList({ resource, ActivityCard, LoadingListComponent }: RecentActivitiesListProps) {
+
+    const { data, isFetching, error, refetch } = useRecentActivities();
+
+    const activitiesList = data?.[resource];
+    const resourceName = resource === "datasetsActivities" ? "Datasets" : "Instructions";
+
+    return (
+        <Card className='w-full h-full p-3'>
+            <CardHeader className='flex-row gap-2 justify-between items-center space-y-0 pt-3 pb-6 px-0'>
+                <CardTitle className='flex gap-2 items-center'>
+                    <DatabaseZapIcon />
+                    Recent {resourceName} Activities
+                </CardTitle>
+            </CardHeader>
+            <Separator className='mb-2' />
+            <CardContent className="flex flex-col gap-3 p-0 pe-1 h-[425px] sm:h-[550px] overflow-auto">
+                {
+                    isFetching ? LoadingListComponent
+                        : error ? <RecentDatasetsActivitiesError error={error} refetch={refetch} /> :
+                            activitiesList?.length ?
+                                ActivityCard(activitiesList)
+                                : (
+                                    <p className='text-center p-4 text-muted-foreground h-full'>
+                                        No Recent {resourceName} Activities
+                                    </p>
+                                )
+                }
+            </CardContent>
+        </Card>
+    )
+}
