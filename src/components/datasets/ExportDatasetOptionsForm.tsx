8d9a0a23103f601e90bbb6366523373bd75fa44f
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-28 20:32:26 +0300

    feat: create `ExportDatasetOptionsForm` component
    Create `ExportDatasetOptionsForm` component which is
    a form provides options of how to export the dataset,
    for now it provides two options, an option for the format
    of the dataset file and anothe one for the handler of
    exporting process.
    
    After the user selects his choices and clicks "Export",
    `onSubmit` function of `useExportDataset` hook will be
    invoked to handle starting exporting dataset based on
    user's choices.

---
+++ src/components/datasets/ExportDatasetOptionsForm.tsx
@@ -0,0 +1,96 @@
+"use client"
+import { Button } from '../ui/button'
+import { DownloadIcon, FileUpIcon } from 'lucide-react'
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '../ui/form'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
+import useExportDataset, { exportDatasetFormOptions } from '@/hook/datasets/useExportDataset'
+
+export default function ExportDatasetOptionsForm({ dataset }: { dataset: Dataset }) {
+
+    const { form, onSubmit } = useExportDataset(dataset);
+
+    return (
+        <Form {...form}>
+            <form className="grid gap-4" onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="space-y-2">
+                    <h4 className="flex gap-1 items-center font-medium leading-none">
+                        <FileUpIcon size={17} />
+                        Export Dataset
+                    </h4>
+                    <p className="text-sm text-muted-foreground">
+                        Choose how to export this dataset into your machine
+                    </p>
+                </div>
+                <FormField
+                    control={form.control}
+                    name="format"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Format</FormLabel>
+                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                <FormControl>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select Format" />
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                    {
+                                        exportDatasetFormOptions.formats.map(
+                                            (format) => <SelectItem value={format}>{format}</SelectItem>
+                                        )
+                                    }
+                                </SelectContent>
+                            </Select>
+                            <FormDescription>
+                                The file format that the dataset will be exported in
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="handler"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Handler</FormLabel>
+                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                <FormControl>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Select Handler" />
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                    {
+                                        exportDatasetFormOptions.handlers.map(
+                                            (handler) => <SelectItem value={handler}>{handler}</SelectItem>
+                                        )
+                                    }
+                                </SelectContent>
+                            </Select>
+                            <FormDescription>
+                                Choose which will handle dataset exporting
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button
+                    size="sm"
+                    type="submit"
+                    className="gap-1"
+                >
+                    <DownloadIcon size={17} /> Export
+                </Button>
+            </form>
+        </Form>
+    )
+}
