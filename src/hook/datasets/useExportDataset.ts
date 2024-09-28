a7d7005c6f3298170814be822ecaca9c7fa46739
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-09-28 20:20:47 +0300

    feat: create `useExportDataset` hook
    Create `useExportDataset` hook which handles exporting
    datasets actions and taking the decision of whether to make
    the browser handles the exporting (installing the dataset
    as file) or to handle the exporting and saving the dataset
    as file into user's file system in the app with tracking the
    state of exporting and displaying realtime UI feedback to
    the user.

---
+++ src/hook/datasets/useExportDataset.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import useExportingDatasetsContext from "../useExportingDatasetsContext";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+export const exportDatasetFormOptions = {
+  formats: ["CSV", "JSONL"],
+  handlers: ["App", "Browser"],
+} as const;
+
+const exportDatasetFormOptionsSchema = z.object({
+  format: z.enum(exportDatasetFormOptions.formats),
+  handler: z.enum(exportDatasetFormOptions.handlers),
+});
+
+export type ExportDatasetOptionsFormType = z.infer<
+  typeof exportDatasetFormOptionsSchema
+>;
+
+export default function useExportDataset(dataset: Dataset) {
+  const form = useForm<ExportDatasetOptionsFormType>({
+    resolver: zodResolver(exportDatasetFormOptionsSchema),
+  });
+
+  const { exportDataset, getDatasetExportingState } =
+    useExportingDatasetsContext();
+
+  function onSubmit(options: ExportDatasetOptionsFormType) {
+    if (options.handler === "App") {
+      const datasetExportingState = getDatasetExportingState(dataset);
+      !datasetExportingState?.isExporting && exportDataset(dataset, options);
+    } else {
+      const baseUrl = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
+      const link = document.createElement("a");
+      link.href = `${baseUrl}/export/${dataset._id}?format=${options.format}`;
+      document.body.appendChild(link);
+      link.click();
+    }
+  }
+
+  return {
+    form,
+    onSubmit,
+  };
+}
