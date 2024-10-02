be43a0ce60d4b41852d18c2d281f71dec64cb47f
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-02 11:06:37 +0300

    feat: add loading state to delete datasets operations
    Use the new `LoadingOverlay` component to display
    loading state UI in `DeleteDatasetButton` when deleting
    a dataset is pending.

--- src/components/datasets/DeleteDatasetButton.tsx
+++ src/components/datasets/DeleteDatasetButton.tsx
@@ -3,6 +3,8 @@ import { TrashIcon } from 'lucide-react'
 import { Button, ButtonProps } from '../ui/button'
 import useDeleteDataset from '@/hook/datasets/useDeleteDataset'
 import ActionWithAlert from '../ActionWithAlert'
+import LoadingOverlay from '../LoadingOverlay'
+import React from 'react'
 
 type DeleteDatasetButton = {
     dataset: Dataset;
@@ -20,26 +22,29 @@ export default function DeleteDatasetButton({
     ButtonProps
 }: DeleteDatasetButton) {
 
-    const { deleteDataset } = useDeleteDataset()
+    const { deleteDataset, isPending } = useDeleteDataset()
 
     return (
-        <ActionWithAlert
-            alertTitle={alertTitle}
-            alertDescription={alertDescription}
-            onAgree={() => {
-                deleteDataset(dataset)
-                onDelete?.()
-            }}
-            trigger={
-                <Button
-                    size="icon"
-                    variant="destructive"
-                    className="size-7 sm:size-8"
-                    {...ButtonProps}
-                >
-                    <TrashIcon size={17} />
-                </Button>
-            }
-        />
+        <>
+            <ActionWithAlert
+                alertTitle={alertTitle}
+                alertDescription={alertDescription}
+                onAgree={() => {
+                    deleteDataset(dataset)
+                    onDelete?.()
+                }}
+                trigger={
+                    <Button
+                        size="icon"
+                        variant="destructive"
+                        className="size-7 sm:size-8"
+                        {...ButtonProps}
+                    >
+                        <TrashIcon size={17} />
+                    </Button>
+                }
+            />
+            {isPending && <LoadingOverlay />}
+        </>
     )
 }
