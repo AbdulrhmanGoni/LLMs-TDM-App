b3725c5124f9d16723e5ad7ce1ac650f745def63
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-10-15 16:19:21 +0300

    feat: create `closeDialog` function in `DialogWrapper.tsx`
    Use `useRef` hook to create a refrence to pass it to close button
    inside `DialogContent` component,
    
    And create `closeDialog` function to use the refrence to trigger
    a click event on close button to close the dialog.
    
    Make the children of `DialogContent` able to access `closeDialog`
    function by passing to `content` prop a function receives as
    parameter an object has `closeDialog` function.

--- src/components/DialogWrapper.tsx
+++ src/components/DialogWrapper.tsx
@@ -3,21 +3,31 @@ import {
     DialogContent,
     DialogTrigger,
 } from "@/components/ui/dialog"
-import { ReactNode } from "react"
+import { ReactNode, useRef } from "react"
 
 type DialogWrapperProps = {
-    content: ReactNode;
+    content: ((params: { closeDialog: () => void }) => ReactNode) | ReactNode;
     trigger: ReactNode;
 }
 
 export default function DialogWrapper({ content, trigger }: DialogWrapperProps) {
+
+    const closeButtonRef = useRef<HTMLButtonElement>(null)
+
+    function closeDialog() {
+        closeButtonRef.current?.click()
+    }
+
     return (
         <Dialog>
             <DialogTrigger asChild>
                 {trigger}
             </DialogTrigger>
-            <DialogContent aria-describedby={undefined}>
-                {content}
+            <DialogContent
+                aria-describedby={undefined}
+                closeButtonRef={closeButtonRef}
+            >
+                {typeof content === "function" ? content({ closeDialog }) : content}
             </DialogContent>
         </Dialog>
     )
