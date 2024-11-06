5a16a0b4994c5c9bb1aca0c5bbdd3a2bda7a2175
AbdulrhmanGoni <abdulrhmangoni@gmail.com>
2024-11-06 17:43:28 +0300

    feat: add "Go to datasets page" button
    Add "Go to datasets page" button to `DatasetsOverviewBox`
    component to provide a clear direct way to take the users to
    datasets page

--- src/components/datasets/DatasetsOverviewBox.tsx
+++ src/components/datasets/DatasetsOverviewBox.tsx
@@ -1,10 +1,11 @@
 "use client"
 import useDatasetsOverview from '@/hook/datasets/useDatasetsOverview';
 import { Card, CardTitle } from '../ui/card'
-import { DatabaseIcon } from 'lucide-react'
+import { ArrowRightIcon, DatabaseIcon } from 'lucide-react'
 import { useRouter } from 'next/navigation';
 import DatasetsOverviewBoxLoading from './DatasetsOverviewBoxLoading';
 import DatasetsOverviewBoxError from './DatasetsOverviewBoxError';
+import { Button } from '../ui/button';
 
 export default function DatasetsOverviewBox() {
 
@@ -15,10 +16,9 @@ export default function DatasetsOverviewBox() {
         isFetching ? <DatasetsOverviewBoxLoading /> :
             error ? <DatasetsOverviewBoxError error={error} refetch={refetch} /> : (
                 <Card
-                    className='flex flex-col justify-center flex-1 h-full p-6 cursor-pointer'
-                    onClick={() => push("/datasets")}
+                    className='flex flex-col justify-center flex-1 h-full p-6 cursor-pointer gap-2'
                 >
-                    <div className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <div className="flex flex-row items-center justify-between space-y-0">
                         <CardTitle className="text-xl font-medium">Datasets</CardTitle>
                         <DatabaseIcon className="h-5 w-5 text-muted-foreground" />
                     </div>
@@ -31,6 +31,19 @@ export default function DatasetsOverviewBox() {
                                     "no datasets added last month"
                             }
                         </p>
+                    </div>
+                    <div className='flex flex-row-reverse'>
+                        <Button
+                            size="sm"
+                            className='gap-1'
+                            onClick={() => push("/datasets")}
+                        >
+                            {
+                                data && data.totalDatasets > 7 ? "Datasets page" :
+                                "Create your first dataset"
+                            }
+                            <ArrowRightIcon size={17} />
+                        </Button>
                     </div>
                 </Card>
             )
